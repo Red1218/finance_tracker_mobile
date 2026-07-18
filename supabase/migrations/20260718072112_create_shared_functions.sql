598fa477c32138a7e0220c6286315f21d6adb9af
Red1218 <venkatphani2@gmail.com>
2026-07-18 12:52:26 +0530

    feat(database): add shared trigger functions

---
+++ supabase/migrations/20260718072112_create_shared_functions.sql
@@ -0,0 +1,11 @@
+-- Shared trigger functions
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$;
